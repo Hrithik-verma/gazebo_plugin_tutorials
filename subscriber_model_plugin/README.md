0c8d25de9fb3d7aae0b0e1d50f571d1608d16a73
hrithik verma <hrithik.verma@drishti.works>
2022-09-30 01:12:32 +0530

    added gazebo subscriber plugin

---
+++ subscriber_model_plugin/README.md
@@ -0,0 +1,23 @@
+## Write A Gazebo World Plugin
+In this tutorial we will understand how to write a gazebo world plugin
+
+
+# ** Running Simulation ** <br />
+**asssuption**: Its assume that your are within ros_workspacew & source the terminals. If not than please change directory to respective workspace & source it with follow command:
+```
+cd <workspace>
+source devel/setup.bash
+```
+
+**In case you made any changes to plugin code, please do**
+```
+catkin_make
+```
+
+**Launch gazebo simulator with plugin**
+```
+roslaunch subscriber_model_plugin gazebo_subscriber.launch
+```
+
+
+[Subscriber Tutorial Document](https://sites.google.com/view/gazebo-plugin-tutorials/4-gazebo-subscriber-plugin)
---
+++ subscriber_model_plugin/src/gazebo_subscriber.cc
@@ -0,0 +1,46 @@
+#include <gazebo/gazebo.hh>                // for accessing all gazebo classes
+#include <gazebo/common/common.hh>         // for common fn in gazebo like WorldPlugin()
+#include <gazebo/physics/physics.hh>       // for gazebo physics, to access -- WorldPtr
+
+
+
+namespace gazebo {
+    class SubscriberGazeboPlugin : public ModelPlugin {
+      public:
+        SubscriberGazeboPlugin() : ModelPlugin() {
+            printf("Subscriber Plugin Created!\n");
+        }
+
+      public:
+        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+            
+            // using _model pointer & GetName()to get model name
+            std::cout << "Model Name = " << _model->GetName() << std::endl;
+
+            // set a node to subscribe
+            transport::NodePtr node(new transport::Node());
+            node->Init();
+
+            // subscribe to topic 
+            this->sub = node->Subscribe("~/world_stats", &SubscriberGazeboPlugin::On_msg, this);
+
+                                                               //equavalent: this->On_msg
+        }
+    
+      public:
+       void On_msg(ConstWorldStatisticsPtr &_msg)
+        {
+          
+          // Dump the message contents to stdout.
+          std::cout << _msg->DebugString();  //print output in the terminal
+          
+        }
+
+     // good pratices to declare pub/sub as data member
+     // reather than declaring them as local varibales which may cause issues
+    private:
+        transport::SubscriberPtr sub; 
+    };
+    // Register plugin 
+    GZ_REGISTER_MODEL_PLUGIN(SubscriberGazeboPlugin)
+}
